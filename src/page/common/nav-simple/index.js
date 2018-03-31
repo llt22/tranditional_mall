7e954f7d4df640c1c5f52e922b468796e4a5a7ba
llt22 <262387013@qq.com>
2018-04-01 04:33:32 +0800

    nav, footer部分开发

---
+++ src/page/common/nav-simple/index.js
@@ -0,0 +1,8 @@
+require('./index.css')
+
+// $(document).ready(function(){
+//     $(document).scroll(function(e) {
+//         console.log(e)
+//         console.log('滚动')
+//     });
+// });
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,12 +1,4 @@
 var util = require('../../util/index')
 require('./index.css')
-
-util.request({
-    url: '/product/list.do?keyword=1',
-    success: function (res) {
-        console.log('res', res)
-    },
-    error: function (err) {
-        console.log('err', err)
-    }
-})
+require('page/common/nav-simple/index')
+require('page/common/nav-normal/index')
