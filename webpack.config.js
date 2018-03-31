b2ad4ce3255f1138211706a2e477ba108c3d954c
llt22 <262387013@qq.com>
2018-03-31 19:40:24 +0800

    配置本地开发服务器

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path')
+var WEBPACK_ENV =  process.env.WEBPACK_ENV || 'dev'
 
 var getHtmlConfig = function (pageName) {
     return {
-        template: './src/view/'+ pageName +'.html',
-        filename: 'view/'+ pageName +'.html',
+        template: './src/view/' + pageName + '.html',
+        filename: 'view/' + pageName + '.html',
         inject: true,
         hash: true,
         // 要引入哪几个模块，因为模块中引入样式，所以样式不用单独写
@@ -20,9 +21,17 @@ var config = {
         'index': ['./src/page/index/index.js'],
         'login': ['./src/page/login/index.js'],
     },
+
+    // devServer: {
+    //     port:8000
+    // },
+
     output: {
+        // 存储编译后的文件的路径
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
+        filename: 'js/[name].js',
+        // 访问文件的路径
+        publicPath: '/dist/'
     },
     externals: {
         'jquery': 'window.jQuery'
@@ -35,17 +44,21 @@ var config = {
         }),
         // 提交 css 到单独文件
         new ExtractTextPlugin("css/[name].css"),
-        new HtmlWebpackPlugin( getHtmlConfig('index')),
-        new HtmlWebpackPlugin( getHtmlConfig('login'))
+        new HtmlWebpackPlugin(getHtmlConfig('index')),
+        new HtmlWebpackPlugin(getHtmlConfig('login'))
     ],
     module: {
         loaders: [
             {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
             // limit=100 小于 100k 的文件会转 base4 格式
             // name=resource/[name].[ext] 放在指定文件夹
-            { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' },
+            {test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]'},
         ]
     }
+}
+
+if(WEBPACK_ENV === 'dev') {
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8081')
 }
 
 module.exports = config
